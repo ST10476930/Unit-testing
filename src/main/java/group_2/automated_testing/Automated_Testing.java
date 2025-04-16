8c899b0b5d8e6e8ea204bd9df2c45733312ae5e8
anele <anele@Nontanda>
2025-04-16 15:20:34 +0200

    Adding Project files

---
+++ src/main/java/group_2/automated_testing/Automated_Testing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package group_2.automated_testing;
+
+/**
+ *
+ * @author anele
+ */
+public class Automated_Testing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
